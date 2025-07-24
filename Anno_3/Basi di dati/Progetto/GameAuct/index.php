21a81c43879b57899e97796ca398191c0b2330b7
Mattia Robuschi Caprara <mattia.r.c@icloud.com>
2025-07-24 23:57:54 +0200

    Aggiunto Progetto in Base di Dati

---
+++ Anno_3/Basi di dati/Progetto/GameAuct/index.php
@@ -0,0 +1,15 @@
+<?php include 'base.html'?>
+        <!--HomePage-->
+
+        <div class="home-page-container">
+            <ul>
+                <li>
+                    <h1 class="home-page1">Dottor Diego Costa</h1>
+                </li>
+                <li>
+                    <h1 class="home-page2">Psicologo</h1>
+                </li>
+            </ul>
+        </div>
+
+<?php include 'footer.html'?>
